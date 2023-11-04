a360068a3f02134f656767d36fefa02c561479ad
Silver <hw16d0@gmail.com>
2023-11-04 19:28:49 -0400

    nooooo mooooore creepers! *vine boom*

--- __main__.py
+++ __main__.py
@@ -1,1 +1,7 @@
-"i'm a creepah, minecraft's grim reapah"
+import os
+if os.getcwd() != os.path.dirname(__file__):
+    os.chdir(os.path.dirname(__file__))
+from . import preferences
+from .gui import window
+wind = window.Window(baseName='spotify playback')
+wind.mainloop()
